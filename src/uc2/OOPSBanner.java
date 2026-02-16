a2411d44b003a9fa2a4bd0428d33b735353b44d3
Pradeep Kumar Mahapatra <PradeepKumarMahapatra@Monalisas-MacBook-Pro-161.local>
2026-02-16 13:39:08 +0530

    Moved uc1 into src folder

---
+++ src/uc2/OOPSBanner.java
@@ -0,0 +1,17 @@
+package uc2;
+
+public class OOPSBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    ******    *****  ");
+        System.out.println("*     *  *     *   *     *  *     * ");
+        System.out.println("*     *  *     *   *     *  *       ");
+        System.out.println("*     *  *     *   ******    *****  ");
+        System.out.println("*     *  *     *   *              * ");
+        System.out.println("*     *  *     *   *        *     * ");
+        System.out.println(" *****    *****    *         *****  ");
+
+    }
+}
+
---
+++ src/uc2/README.md
@@ -0,0 +1,12 @@
+# UC2 - OOPS Banner App
+
+## Objective
+Print the word "OOPS" in large banner format using Java print statements without using loops.
+
+## Requirements
+- Use only System.out.println()
+- Do not use loops
+- Print banner in 7 lines using '*' and spaces
+
+## File Structure
+
